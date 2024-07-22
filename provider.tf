4fb2a9d8154fa09afe2a4dcdfef8fc858ea87732
Ved Janardhan <vedaprasad.sf@gmail.com>
2024-07-22 19:58:21 +1000

    Change coment

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "argocd" {
  server_addr = "argo.ved-tech.net"
   username   = "admin"
-  password   = "xxxxxxxx" #### ask ved for password
+  password   = "xxxxxxxx" #### ask ved for secretpassword
 }
